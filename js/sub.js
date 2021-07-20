9668cee7d6398127b0917b1564c17b162b2a9016
BeautyJeong <wjdqbxl@gachon.ac.kr>
2021-07-20 15:36:50 +0900

    0720 skill, contact
    15:30

--- js/sub.js
+++ js/sub.js
@@ -5,12 +5,15 @@ $(document).ready(function () {
     const stickyY = $('.sticky').offset().top;
     console.log(winHei, stickyY);
 
-    $('.tree_wrap .seed:nth-of-type(1)').addClass('on');
     $(window).on('scroll', function () {
         const scrollY = $(this).scrollTop();
         $('.tree_wrap > div').each(function (idx) {
             if(scrollY > stickyY + winHei * idx) $(this).addClass('on').siblings().removeClass('on');
         });
     });
+
+    //ie브라우저 9+ 추가지원
+    const elements = $('.sticky');
+    Stickyfill.add(elements);
 
 });
